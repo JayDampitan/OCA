128a3dd7a082afec20b19d9d70827b796f7a21b2
JayDampitan <jaykennethpd@gmail.com>
2023-09-15 12:25:18 -0700

    install new nextjs, install dependencies and apply navigation for desktop and mobile

---
+++ oca-gym/assets/data.js
@@ -0,0 +1,22 @@
+export const NAV_ITEMS = [
+    {
+      label: "Home",
+      page: "/home",
+    },
+    {
+      label: "About",
+      page: "/about",
+    },
+    {
+      label: "Services",
+      page: "/services",
+    },
+    {
+      label: "Facility",
+      page: "/facility",
+    },
+    {
+      label: "Contact Us",
+      page: "/contact",
+    },
+  ];
