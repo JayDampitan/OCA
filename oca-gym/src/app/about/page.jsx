373ec5386165f75deadc36d7ab9815d74b22b55d
JayDampitan <jaykennethpd@gmail.com>
2023-10-08 10:22:42 -0700

    added new images on about page and fix minor responsiveness.

--- oca-gym/src/app/about/page.jsx
+++ oca-gym/src/app/about/page.jsx
@@ -1,17 +1,17 @@
-
-import Image from 'next/image'
-import aboutMain from "../../../assets/aboutMain.jpg"
-
+import Image from "next/image";
+import aboutMain from "../../../assets/aboutMain.jpg";
+import Champ from "../../../assets/JacobAndChamp.jpg";
+import Ringside from "../../../assets/ringside.jpg";
 
 function About() {
   return (
-    <div className="text-center bg-bodyBackground text-offWhite flex items-center flex-col justify-center h-[100vh] w-full">
-      <div className="bg-primary w-full flex-col flex items-center justify-center py-24 lg:py-44">
+    <div className="text-center bg-bodyBackground text-offWhite flex items-center flex-col justify-center h-auto w-full ">
+      <div className="bg-primary w-full flex-col flex items-center justify-center pt-36 pb-20 lg:py-44">
         <h2 className="  text-offWhite uppercase text-lg lg:text-4xl font-semibold">
           welcome ochoa combat academy
         </h2>
         <p className="max-w-3xl text-sm text-offWhite mt-10 sm:text-xl px-2 leading-6 lg:leading-10">
-         {` Welcome to our family-friendly combat gym, where we're dedicated to fostering not only martial arts skills but also strong family bonds. At our gym, we believe that fitness and self-defense should be accessible to everyone, from kids to grandparents, and we've created an inclusive environment to make that possible. Our experienced and certified instructors are passionate about helping families grow together through the discipline and camaraderie of martial arts.`}
+          {` Welcome to our family-friendly combat gym, where we're dedicated to fostering not only martial arts skills but also strong family bonds. At our gym, we believe that fitness and self-defense should be accessible to everyone, from kids to grandparents, and we've created an inclusive environment to make that possible. Our experienced and certified instructors are passionate about helping families grow together through the discipline and camaraderie of martial arts.`}
         </p>
       </div>
 
@@ -24,13 +24,43 @@ function About() {
           <div className="w-[100%] lg:w-[100%] bg-primary p-2">
             <Image
               src={aboutMain}
-              // width="70%"
-              // height="70%"
               alt="Image of the Ochoa Combat Gym members"
-              className="w-fullz h-full object-cover"
+              className="w-full h-full object-cover"
             />
           </div>
-          
+        </div>
+      </div>
+
+      <div className="w-full flex items-center justify-center flex-col">
+        <Image src={Champ} className="w-60 md:w-96 rounded-sm md:rounded-md" />
+        <h3 className="my-4 text-2xl px-8 font-bold">
+          MEET OUR NEW CHAMPION GABRIEL REYES!!
+        </h3>
+      </div>
+
+      <div className="my-10 w-[90%] md:w-[50%] space-y-8 flex items-center justify-center flex-col ">
+        <h3 className="my-4 text-xl font-bold px-8  border-b-2 py-2 border-primary">
+          WATCH HIS FIGHTS HIGHLIGHTS!!
+        </h3>
+        {/* <iframe
+          className="w-80 md:w-full h-full"
+         
+          src="https://www.youtube.com/embed/l3BZnMA9xbQ?si=Vto6P44H8I_sSKfA"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe> */}
+        <div className="relative overflow-hidden pt-[56.25%] w-full h-full">
+          <iframe
+            className="absolute w-full h-full top-0"
+         
+            src="https://www.youtube.com/embed/l3BZnMA9xbQ?si=Vto6P44H8I_sSKfA"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
         </div>
       </div>
     </div>
--- oca-gym/src/app/facility/page.jsx
+++ oca-gym/src/app/facility/page.jsx
@@ -15,26 +15,26 @@ function Facility() {
         <h2 className="border-b border-offWhite uppercase text-lg lg:text-4xl font-semibold">
           facility
         </h2>
-        <p className="text-[13px] lg:text-xl px-10 mt-8 lg:max-w-4xl leading-6 lg:leading-10">
+        <p className="text-[16px] lg:text-xl px-10 mt-8 lg:max-w-4xl leading-6 lg:leading-10">
           {`   Our facility is equipped with top-notch training mats, punching bags, and a variety of other training equipment to ensure you have the tools you need for a successful journey in martial arts. We offer a wide range of classes that cater to different interests and skill levels, allowing you to choose the path that suits you best. Whether you're a seasoned martial artist or a beginner, you'll find a community of like-minded individuals here to support you on your martial arts journey. Join us today, and experience the camaraderie and growth that come with training at our friendly combat gym!`}
         </p>
       </div>
       <Image
         alt="image of ochoa combat gym"
         src={facility1}
-        className="h-64 lg:h-auto lg:w-[70%] -translate-y-8 lg:-translate-y-16 object-cover rounded-sm"
+        className="w-[90%] rounded-sm lg:h-auto lg:w-[70%] -translate-y-8 lg:-translate-y-16 object-cover"
       />
 
-      <div className="w-full p-2 lg:pl-50 md:flex-row flex flex-col justify-around items-center my-8">
-        <Image alt="image of ochoa combat gym mat" src={facility2} className="w-[50%] md:w-[40%]"/>
-        <p className="p-10 flex items-center md:text-3xl text-offWhite md:w-[50%]">
-            {`Forge Warriors on Our High-Quality Training Mats: Join Our Combat Academy for a Foundation of Excellence! Our facility boasts the perfect foundation for your martial arts journey with top-tier, shock-absorbent training mats. Train confidently, knowing you're on the same mats trusted by champions!`}.
+      <div className="w-[90%] p-2 lg:pl-50 md:flex-row flex flex-col justify-around items-center my-8">
+        <Image alt="image of ochoa combat gym mat" src={facility2} className="rounded-sm md:rounded-md w-[50%] md:w-[40%]"/>
+        <p className="p-10  flex items-center text-center md:text-3xl text-offWhite md:w-[50%]">
+            {`Forge Warriors on Our High-Quality Training Mats: Join Our Combat Academy for a Foundation of Excellence! Our facility boasts the perfect foundation for your martial arts journey with top-tier, shock-absorbent training mats.`}.
         </p>
       </div>
-      <div className="w-full p-2 lg:pr-50 md:flex-row-reverse flex flex-col justify-around items-center my-8">
-        <Image alt="image of ochoa combat gym punching bags" src={facility3} className="w-[50%] md:w-[40%]"/>
-        <p className="p-10 flex items-center md:text-3xl text-offWhite md:w-[50%]">
-            {`Unleash Your Potential: Train with Intensity on Our Top-of-the-Line Punching Bags at Our Combat Academy! Our facility is equipped with a diverse range of punching bags, catering to all levels of martial artists. Experience the power and precision of your strikes as you transform into a formidable fighter.`}.
+      <div className="w-[90%] p-2 lg:pr-50 md:flex-row-reverse flex flex-col justify-around items-center my-8">
+        <Image alt="image of ochoa combat gym punching bags" src={facility3} className="rounded-sm md:rounded-md w-[50%] md:w-[40%]"/>
+        <p className="p-10 flex items-center text-center md:text-3xl text-offWhite md:w-[50%]">
+            {`Unleash Your Potential: Train with Intensity on Our Top-of-the-Line Punching Bags at Our Combat Academy! Our facility is equipped with a diverse range of punching bags, catering to all levels of martial artists.`}.
         </p>
       </div>
     
